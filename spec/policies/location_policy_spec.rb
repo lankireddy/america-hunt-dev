e6195c4fe708d05eefe8d67aa30afdb1f106c550
Lee Dykes <lee.dykes@metova.com>
2016-08-18 09:29:14 -0500

    pundit spec

---
+++ spec/policies/location_policy_spec.rb
@@ -0,0 +1,19 @@
+describe LocationPolicy do
+  let(:user) { Fabricate :user }
+  let(:location) { Fabricate :location}
+
+  subject { described_class }
+
+  permissions :show?, :new? do
+    it { is_expected.to permit(user, location) }
+  end
+
+  permissions :new? do
+    it { is_expected.to permit(user) }
+  end
+
+  permissions :create? do
+    it { is_expected.to permit(user) }
+  end
+
+end
