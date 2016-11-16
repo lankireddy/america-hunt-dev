82350ea33d46b5b71fe1a7bbe794e03a523bf2b5
Riley Mills <riley.mills@metova.com>
2016-11-15 18:34:18 -0600

    tweaking sitemap config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -28,4 +28,24 @@ SitemapGenerator::Sitemap.create do
   Page.find_each do |page|
     add page_path(page), lastmod: page.updated_at
   end
+
+  Location.states.each do |st|
+    #[properties['name'], abbreviation]
+    add "states/#{st[1]}/locations"
+  end
+
+  Location.approved.find_each do |loc|
+    add location_path(loc), lastmod: loc.updated_at
+  end
+
+  BlogCategory.find_each do |blog_cat|
+    add blog_category_path(blog_cat), lastmod: blog_cat.updated_at
+  end
+
+  add posts_path, priority: 0.8, changefreq: 'daily'
+
+  Post.find_each do |post|
+    add post_path(post), lastmod: post.updated_at
+  end
+
 end
