ba70681c9d0b58ca3e06388834433fe4e71332a3
Riley Mills <riley.mills@metova.com>
2016-11-17 13:38:53 -0600

    Updates to support heroku

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,29 +1,17 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://americahunt.com/"
 
+#Comment these lines for local testing
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.sitemaps_host = "http://america-hunt-dev.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                                                    fog_directory: 'america-hunt-dev',
+                                                                    fog_region: 'us-west-2')
+
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 
   Page.find_each do |page|
     add page_path(page), lastmod: page.updated_at
