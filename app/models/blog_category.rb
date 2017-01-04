b8e77f71ee59c1cdbd6b309ba6d8efbdbbea8805
Edward <edward.chen@metova.com>
2017-01-04 13:57:56 -0500

    Field notes

--- app/models/blog_category.rb
+++ app/models/blog_category.rb
@@ -30,8 +30,8 @@ class BlogCategory < ActiveRecord::Base
     BlogCategory.where(name: 'Hunting Organizations').first
   end
 
-  def self.field_notes_to_game_wardens_category
-    BlogCategory.where(name: 'Field Notes to Game Wardens').first
+  def self.field_notes_from_game_wardens_category
+    BlogCategory.where(name: 'Field Notes from Game Wardens').first
   end
 
 end
