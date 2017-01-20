4441e7f1b440a346222b409dfe673b063bc57399
Lee Dykes <lee.dykes@metova.com>
2017-01-20 10:11:08 -0600

    refactoring the homepage post assignments

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -6,36 +6,37 @@ class HomePageController < ApplicationController
   end
 
   def new_home
-    collate_posts_for_home
-
+    set_categories
+    if @primary_category
+      collate_posts_for_home
+    else
+      @posts = Post.all.limit(10)
+    end
   end
 
-  def collate_posts_for_home
+  def set_categories
     @primary_category = BlogCategory.primary_category
     @wildlife_category = BlogCategory.wildlife_category
     @hunting_org_category = BlogCategory.hunting_org_category
     @field_notes_from_game_wardens_category = BlogCategory.field_notes_from_game_wardens_category
     @secondary_featured_categories = [@field_notes_from_game_wardens_category, @wildlife_category]
-    if @primary_category
-      featured_ids = []
-
-      @primary_featured = Post.with_featured_position(:primary_featured).first || @primary_category.posts.order(:position).limit(1).first
-      featured_ids << @primary_featured.id if @primary_featured
-      @secondary_featured_one = Post.with_featured_position(:secondary_featured_one).first || @primary_category.posts.where.not(id: featured_ids).order(:position).limit(1).first
-      featured_ids << @secondary_featured_one.id if @secondary_featured_one
-      @secondary_featured_two = Post.with_featured_position(:secondary_featured_two).first || @primary_category.posts.where.not(id: featured_ids).order(:position).limit(1).first
-      featured_ids << @secondary_featured_two.id if @secondary_featured_two
-      @secondary_featured_three = Post.with_featured_position(:secondary_featured_three).first || @primary_category.posts.where.not(id: featured_ids).order(:position).limit(1).first
-      featured_ids << @secondary_featured_three.id if @secondary_featured_three
-
-      @posts = @primary_category.posts.where.not(id: featured_ids).order(:position).limit(10)
-    else
-      @primary_featured = nil
-      @secondary_featured_one = nil
-      @secondary_featured_two = nil
-      @secondary_featured_three = nil
-      @posts = Post.all.limit(10)
-    end
   end
 
+  def collate_posts_for_home
+    @featured_ids = []
+    [:primary_featured, :secondary_featured_one, :secondary_featured_two, :secondary_featured_three].each do |key|
+      featured_post = featured_post(key)
+      instance_variable_set("@#{key}", featured_post)
+      @featured_ids << featured_post.id if featured_post
+    end
+    @posts = not_used.order(:position).limit(10)
+  end
+
+  def not_used
+    @primary_category.posts.where.not(id: @featured_ids)
+  end
+
+  def featured_post(key)
+    Post.with_featured_position(key).first || not_used.order(:position).limit(1).first
+  end
 end
