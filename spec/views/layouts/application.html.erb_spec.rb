8a8cee39d318cd5ebb894e24b8b02503a4ec621b
Karla Pollowitz <karla.pollowitz@metova.com>
2017-02-02 15:58:33 -0800

    move render to before

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -7,34 +7,30 @@ RSpec.describe 'layouts/application', type: :view do
     @location = Fabricate :location
     @page_title = 'America Hunt: ' + @location.name
     @page_description = @location.excerpt
+    render
   end
 
   it 'has metas for title and description' do
-    render
     expect(rendered).to have_selector('title', text: @page_title, visible: false)
     expect(rendered).to have_selector("meta[name='description'][content='#{@location.excerpt}']", visible: false)
   end
 
   it 'displays top ad' do
     pending('this needs to be redone')
-    render
     expect(rendered).to have_selector('.ad-header img')
   end
 
   it 'displays the standard nav bar' do
-    render
     expect(rendered).to have_selector('nav.navbar-inverse')
   end
 
   it 'has a link for each blog category page' do
-    render
     BlogCategory.all.each do |blog_category|
       expect(rendered).to have_selector("a[href='#{blog_category_path(blog_category)}']")
     end
   end
 
   it 'has a link for each page' do
-    render
     expect(rendered).to have_selector("a[href='#{page_path('about')}']")
     expect(rendered).to have_selector("a[href='#{page_path('press-&amp;-media')}']")
     expect(rendered).to have_selector("a[href='#{page_path('faq')}']")
