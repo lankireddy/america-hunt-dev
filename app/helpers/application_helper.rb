64ad75f126ed6ff1805199d0b6080378c5205e1d
Karla Pollowitz <karla.pollowitz@metova.com>
2017-02-06 12:47:42 -0800

    remove unused link_to_active_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-  def link_to_active_class(name, options = {}, html_options = {}, &block)
-    html_options[:class] = html_options[:class].to_s + ' active' if current_page?(options.to_s)
-    link_to name, options, html_options, &block
-  end
-
   def li_link_to_active_class(name, options = {}, html_options = {})
     html_options[:class] = html_options[:class].to_s + ' active' if current_page?(options.to_s)
     "<li class=\"#{html_options[:class]}\">#{link_to name, options}</li>".html_safe
