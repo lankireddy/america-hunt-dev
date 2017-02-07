b7ec9de27787950b66cd222feb82e8da35d8c0ea
Karla Pollowitz <karla.pollowitz@metova.com>
2017-02-07 10:45:40 -0800

    Add helpers to generate dropdowns and active links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,28 @@
 module ApplicationHelper
   def li_link_to_active_class(name, options = {}, html_options = {})
     html_options[:class] = html_options[:class].to_s + ' active' if current_page?(options.to_s)
-    "<li class=\"#{html_options[:class]}\">#{link_to name, options}</li>".html_safe
+    content_tag(:li, link_to(name, options), class: html_options[:class])
+  end
+
+  def li_dropdown_link_to_active_class(name, pages = {}, html_options = {})
+    html_options[:class] = html_options[:class].to_s.present? ? html_options[:class].to_s + ' dropdown' : 'dropdown'
+    html_options[:class] = html_options[:class].to_s + ' active' if active_dropdown(pages)
+    content_tag(:li, a_dropdown(name) + ul_dropdown(pages), class: html_options[:class])
+  end
+
+  def ul_dropdown(pages = {})
+    content_tag :ul, class: 'dropdown-menu' do
+      pages.collect { |page| concat(content_tag(:li, link_to(page[:title], page[:path]))) }
+    end
+  end
+
+  def a_dropdown(name)
+    link_to '#', class: 'dropdown-toggle', data: { toggle: 'dropdown' }, role: 'button', aria: { haspopup: 'true', expanded: 'false' } do
+      (name + ' <span class="caret"></span>').html_safe
+    end
+  end
+
+  def active_dropdown(pages)
+    pages.any? { |page| current_page?(page[:path]) }
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  let!(:pages) { [{ title: 'About', path: page_path('about') }, { title: 'Press & Media', path: page_path('press-&-media') }, { title: 'FAQ', path: page_path('faq') }, { title: 'Terms of Service', path: page_path('terms-of-service') }, { title: 'Privacy Policy', path: page_path('privacy-policy') }] }
+  let!(:name) { 'About Us' }
+
+  describe '#li_link_to_active_class' do
+    it 'returns the li' do
+      expect(helper.li_link_to_active_class 'Send Us News', '/send-us-news').to eq "<li>#{link_to 'Send Us News', '/send-us-news'}</li>"
+    end
+  end
+
+  describe '#li_dropdown_link_to_active_class' do
+    it 'returns the dropdown' do
+      dropdown_text =   '<li class="dropdown">
+                          <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="#">About Us <span class="caret"></span></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/press-&amp;-media">Press &amp; Media</a></li>
+                            <li><a href="/faq">FAQ</a></li>
+                            <li><a href="/terms-of-service">Terms of Service</a></li>
+                            <li><a href="/privacy-policy">Privacy Policy</a></li>
+                          </ul>
+                        </li>'.squish.strip.gsub(/> </, '><')
+      expect(helper.li_dropdown_link_to_active_class name, pages).to eq dropdown_text.squish.strip.gsub(/> </, '><')
+    end
+  end
+
+  describe '#ul_dropdown' do
+    it 'returns the ul with a li for each page' do
+      ul_text = '<ul class="dropdown-menu">
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/press-&amp;-media">Press &amp; Media</a></li>
+                  <li><a href="/faq">FAQ</a></li>
+                  <li><a href="/terms-of-service">Terms of Service</a></li>
+                  <li><a href="/privacy-policy">Privacy Policy</a></li>
+                </ul>'.squish.strip.gsub(/> </, '><')
+      expect(helper.ul_dropdown pages).to eq ul_text.squish.strip.gsub(/> </, '><')
+    end
+  end
+
+  describe '#a_dropdown' do
+    it 'returns the link for the dropdown itself' do
+      a_text = '<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="#">About Us <span class="caret"></span></a>'.squish.strip.gsub(/> </, '><')
+      expect(helper.a_dropdown name).to eq a_text.squish.strip.gsub(/> </, '><')
+    end
+  end
+end
